3fe77a76dc6db4479b3872060a119c882b6bf541
Daniel <dpd144@alumnos.unican.es>
2022-04-27 21:19:37 +0200

    refactor the code. Create a struct (RoundVec) to avoid the use of HashMap and pointers. Now it's really clean & elegant :)

--- src/allocator/islip.rs
+++ src/allocator/islip.rs
@@ -1,18 +1,63 @@
 use std::cell::RefCell;
-use std::collections::HashMap;
+use std::vec;
 
 use rand::rngs::StdRng;
 
-//use quantifiable_derive::Quantifiable;//the derive macro
 use crate::allocator::{Allocator, AllocatorBuilderArgument, GrantedRequests, Request};
 use crate::config_parser::ConfigurationValue;
 use crate::match_object_panic;
 
-
-#[derive(Clone)]
-struct IslipRequest {
-    port: Option<usize>,
+#[derive(Clone, Debug)]
+struct RoundVec {
+    /// The client with the highest priority in the round.
+    pointer : usize,
+    /// Have the indices of the clients that have been requested
+    pub clients : Vec<usize>,
+    /// The number of clients
+    n : usize,
 }
+
+impl RoundVec {
+    fn new(size : usize) -> RoundVec {
+        RoundVec {
+            pointer : 0,
+            clients : Vec::with_capacity(size),
+            n : size,
+        }
+    }
+    /// Add an element to the round vector
+    fn add (&mut self, element : usize) {
+        self.clients.push(element);
+    }
+    /// Increment the pointer
+    fn increment_pointer(&mut self) {
+        self.pointer = (self.pointer + 1) % self.n;
+    }
+    /// Sort the clients using the pointer as the pivot
+    fn sort(&mut self) {
+        // We need to extract the pivot and the size because we only can have one mutable reference
+        let pointer = self.pointer;
+        let size = self.size();
+        self.clients.sort_unstable_by_key(| k|
+            if *k < pointer {
+                *k + size
+            } else {
+                *k
+            }
+        );
+    }
+    /// Get the size of the round vector
+    fn size (&self) -> usize {
+        self.n
+    }
+    /// Check if the round vector is empty
+    /// # Returns
+    /// `true` if the round vector is empty, `false` otherwise
+    fn is_empty(&self) -> bool {
+        self.clients.is_empty()
+    }
+}
+
 
 /// An iSLIP allocator, more info 'https://doi.org/10.1109/90.769767'
 pub struct IslipAllocator {
@@ -22,18 +67,14 @@ pub struct IslipAllocator {
     num_resources: usize,
     /// The number of iterations to perform
     num_iterations: usize,
-    /// The input requests
-    input_requests: Vec<HashMap<usize, IslipRequest>>,
-    /// The output requests
-    output_requests: Vec<HashMap<usize, IslipRequest>>,
     /// The input match
     in_match: Vec<Option<usize>>,
     /// The output match
     out_match: Vec<Option<usize>>,
-    /// The grant pointers
-    g_ptrs: Vec<usize>,
-    /// The accept pointers
-    a_ptrs: Vec<usize>,
+    /// The input requests (RoundVec)
+    in_requests: Vec<RoundVec>,
+    /// The output requests (RoundVec)
+    out_requests: Vec<RoundVec>,
 }
 
 impl IslipAllocator {
@@ -44,7 +85,7 @@ impl IslipAllocator {
     /// * `IslipAllocator` - The new iSLIP allocator
     pub fn new(args: AllocatorBuilderArgument) -> IslipAllocator {
         // Check if the arguments are valid
-        if args.num_clients <= 0 || args.num_resources <= 0 {
+        if args.num_clients == 0 || args.num_resources == 0 {
             panic!("Invalid arguments for IslipAllocator");
         }
         // Get the number of iterations to perform
@@ -62,23 +103,16 @@ impl IslipAllocator {
             println!("Warning: num_iter for the iSLIP allocator is not specified in the configuration file, the default value (1) will be used");
         }
         let num_iterations = num_iterations.unwrap_or(1);
-
-        let input_requests = vec![HashMap::new(); args.num_clients];
-        let output_requests = vec![HashMap::new(); args.num_resources];
         let in_match = vec![None; args.num_clients];
         let out_match = vec![None; args.num_resources];
-        let g_ptrs = vec![0; args.num_clients];
-        let a_ptrs = vec![0; args.num_resources];
         IslipAllocator {
             num_clients: args.num_clients,
             num_resources: args.num_resources,
             num_iterations,
-            input_requests,
-            output_requests,
             in_match,
             out_match,
-            g_ptrs,
-            a_ptrs,
+            in_requests: vec![RoundVec::new(args.num_resources); args.num_clients],
+            out_requests: vec![RoundVec::new(args.num_clients); args.num_resources],
         }
     }
 
@@ -98,58 +132,30 @@ impl IslipAllocator {
 }
 
 impl Allocator for IslipAllocator {
+    /// Add a request to the allocator
+    /// # Arguments
+    /// * `request` - The request to add
+    /// # Remarks
+    /// The request is valid if the client is in the range [0, num_clients) and the resource is in the range [0, num_resources)
+    /// Asume that the request is is not already in the allocator
+    /// # Panics
+    /// If the request is not valid
     fn add_request(&mut self, request: Request) {
         // Check if the request is valid
         if !self.is_valid_request(&request) {
             panic!("The request is not valid");
         }
-        // Check if the request is already in the allocator
-        // FIXME: (Paranoia Mode ON)
-        //  Maybe we din't need to check this, because the router will not send the same request twice
-        if self.input_requests[request.client].contains_key(&request.resource) {
-            panic!("The request is already in the input requests");
-        }
-        if self.output_requests[request.resource].contains_key(&request.client) {
-            panic!("The request is already in the output requests");
-        }
-        let in_port = request.client;
-        let out_port = request.resource;
+        
+        // The in_requests and out_requests are indexed by the client/resource respectively
+        // We need to get the index of the client/resource
+        let client = request.client;
+        let resource = request.resource;
 
-        // Create the iSLIP request
-        let mut req = IslipRequest {
-            port: Some(out_port),
-        };
-
-        // add the request to the input requests
-        self.input_requests[in_port].insert(out_port, req.clone());
-        // add the request to the output requests
-        req.port = Some(in_port);
-        self.output_requests[out_port].insert(in_port, req);
+        // Add the request to the input requests and output requests
+        // Asume that the request is not already in the requests vectors
+        self.in_requests[client].add(resource);
+        self.out_requests[resource].add(client);
     }
-
-  
-
-    //         Create the granted requests vector and return it
-    //         let mut gr = GrantedRequests {
-    //             granted_requests: vec![],
-    //         };
-    //         for output in 0..self.num_resources {
-    //             if grants[output].is_some() {
-    //                 let req = Request {
-    //                     client: grants[output].unwrap(),
-    //                     resource: output,
-    //                     priority: Some(0),
-    //                 };
-    //                 gr.granted_requests.push(req);
-    //             }
-    //         }
-    //         // TODO: Clean up things!!
-    //         let mut gr = GrantedRequests {
-    //             granted_requests: vec![],
-    //         };
-    //         return gr;
-    //     }
-    // }
 
     /// Perform the allocation
     /// # Arguments
@@ -157,29 +163,110 @@ impl Allocator for IslipAllocator {
     /// # Returns
     /// * `GrantedRequests` - The granted requests
     fn perform_allocation(&mut self, _rng: &RefCell<StdRng>) -> GrantedRequests {
-        // the input port
-        let mut input: usize;
-        // the output port
-        let mut output: usize;
-        // the input offset
-        let mut input_offset: usize;
-        // the output offset
-        let mut output_offset: usize;
-        // the iterator for the input/output requests lists
-        let mut iter;
-        // the wrapped flag
-        // is used to check if we have wrapped around the list
-        let mut wrapped;
-        // the granted requests vector
-        let mut grants = vec![None; self.num_resources];
 
+        // Create the granted requests vector
+        //FIXME: igual hay que cambiar el tipo GrantedRequests para que sea un alias de un vector de Request
+        let mut gr = GrantedRequests { granted_requests: Vec::new() };
+
+        // Sort the input requests
+        for client in 0..self.num_clients {
+            self.in_requests[client].sort();
+        }
+        // Sort the output requests
+        for resource in 0..self.num_resources {
+            self.out_requests[resource].sort();
+        }
+
+        // Reset the in_match and out_match vectors
+        for client in 0..self.num_clients {
+            self.in_match[client] = None;
+        }
+        for resource in 0..self.num_resources {
+            self.out_match[resource] = None;
+        }
+        
         for islip_iter in 0..self.num_iterations {
+            // the granted requests vector
+            // (Indexed by the resource)
+            let mut grants = vec![None; self.num_resources];
+
             // Grant phase
-            for output in 0..self.num_resources {
-                // skip if the output is already matched OR if there is no request for the output
-                if self.out_match[output].is_some() || self.output_requests[output].is_empty() {
+            for resource in 0..self.num_resources {
+                // skip if the output is already matched OR if there is no any request for the output
+                if self.out_match[resource].is_some() || self.out_requests[resource].is_empty() {
                     continue;
                 }
+                
+                for request in &self.out_requests[resource].clients {
+                    let client = *request;
+                    // know that the output is not matched (see above) and 
+                    // if the input is free (no match) then grant the request
+                    if self.in_match[client].is_none() {
+                        grants[resource] = Some(client);
+                        break; // break the inner loop
+                    }
+                }
+            } // end of the GRANT phase
+
+            // Accept phase
+            for client in 0..self.num_clients {
+                // skip if the there is no any request for the input
+                if self.in_requests[client].is_empty() {
+                    continue;
+                }
+                
+                for request in &self.in_requests[client].clients {
+                    let resource = *request;
+
+                    if grants[resource] == Some (client) {
+                        self.out_match[resource] = Some(client);
+                        self.in_match[client] = Some(resource);
+                        break; // break the inner loop
+                    }
+
+                    // we know the output is free (above) and
+                    // if the input is free, grant request
+                    if grants[resource] == Some(client) {
+                        // accept
+                        self.in_match[client] = Some(resource);
+                        self.out_match[resource] = Some(client);
+                        // add the request to the granted requests
+                        let req = Request {
+                            client,
+                            resource,
+                            priority: Some(0),
+                        };
+                        gr.granted_requests.push(req);
+                        
+                        // only update pointers if accepted during the 1st iteration
+                        if islip_iter == 0 {
+                            // update the input/output requests lists pointers
+                            self.in_requests[client].increment_pointer();
+                            self.out_requests[resource].increment_pointer();
+                        }
+                        break;
+                    }
+                }
+            } // end of the ACCEPT phase
+        } // end of the ITERATIONS phase
+        // return the granted requests
+        gr
+    }
+
+
+    /// OLD VERSION
+/*     ///  for islip_iter in 0..self.num_iterations {
+            // Grant phase
+            for output in 0..self.num_resources {
+                // if self.out_match[output].is_some() || self.output_requests[output].is_empty() {
+                //     continue;
+                // }
+                
+                // skip if the output is already matched OR if there is no request for the output
+                if self.out_match[output].is_some() || self.out_requests[output].is_empty() {
+                    continue;
+                }
+                
                 // a round-robin arbiter between the input requests
                 input_offset = self.g_ptrs[output];
 
@@ -286,7 +373,8 @@ impl Allocator for IslipAllocator {
             }
         }
         gr
-    }
+    } */
+
 
     fn support_intransit_priority(&self) -> bool {
         false
